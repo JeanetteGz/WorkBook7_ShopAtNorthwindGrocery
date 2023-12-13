7b489ffe9208a6bb9ce1a647460a271473f0f749
Jeanettegz <Jeanetteglz99@gmail.com>
2023-12-12 21:28:46 -0500

    Got view all to work

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,42 +1,44 @@
-let mainDropdownSelect = document.getElementById("searchOption");
-let categoryDropdownOptions = document.getElementById("category");
-let productsList = document.getElementById("results");
+window.onload = () => {
+    let mainDropdownSelect = document.getElementById("searchOption");
+    let categoryDropdownOptions = document.getElementById("category");
+    let productsList = document.getElementById("results");
 
-mainDropdownSelect.addEventListener("change", () => {
-    if (mainDropdownSelect.value === "searchByCategory") {
-        fetch("http://localhost:8081/api/categories")
-            .then((res) => res.json())
-            .then((allCategories) => {
-                categoryDropdownOptions.innerHTML = ''; // Clear previous options
-                for (let category of allCategories) {
-                    var optionElement = document.createElement("option");
-                    optionElement.textContent = category.name;
-                    optionElement.value = category.categoryId;
-                    categoryDropdownOptions.appendChild(optionElement);
-                }
-            })
-    }
-    if (mainDropdownSelect.value === "viewAll") {
+    mainDropdownSelect.addEventListener("change", () => {
+        if (mainDropdownSelect.value === "searchByCategory") {
+            fetch("http://localhost:8081/api/categories")
+                .then((res) => res.json())
+                .then((allCategories) => {
+                    categoryDropdownOptions.innerHTML = ''; // Clear previous options
+                    for (let category of allCategories) {
+                        var optionElement = document.createElement("option");
+                        optionElement.textContent = category.name;
+                        optionElement.value = category.categoryId;
+                        categoryDropdownOptions.appendChild(optionElement);
+                    }
+                });
+        }
+        if (mainDropdownSelect.value === "viewAll") {
+            fetch("http://localhost:8081/api/products")
+                .then((res) => res.json())
+                .then((allProducts) => {
+                    productsList.innerHTML = ' '; // Clear previous content
+                    for (let product of allProducts) {
+                        productsList.innerHTML += `${product.productName} <a href="${product.productId}">${product.productName}</a><br>`;
+                    }
+                });
+        }
+    });
+
+    categoryDropdownOptions.addEventListener("change", () => {
+        let selectedCategory = categoryDropdownOptions.value;
         fetch("http://localhost:8081/api/products")
             .then((res) => res.json())
             .then((allProducts) => {
-                productsList.innerHTML = ' ';
-                for (let product of allProducts) {
-                    productsList.innerHTML += `${product.productName} <a href="${product.productId}">${product.productName}</a><br>`;
+                let products = allProducts.filter(val => val.categoryId == selectedCategory);
+                productsList.innerHTML = ' '; // Clear previous content
+                for (let product of products) {
+                    productsList.innerHTML += `${product.productName} <br>`;
                 }
             });
-    }
-});
-
-categoryDropdownOptions.addEventListener("change", () => {
-    let selectedCategory = categoryDropdownOptions.value;
-    fetch("http://localhost:8081/api/products")
-        .then((res) => res.json())
-        .then((allProducts) => {
-            let products = allProducts.filter(val => val.categoryId == selectedCategory);
-            productsList.innerHTML = ' ';
-            for (let product of products) {
-                productsList.innerHTML += `${product.productName} <br>`;
-            }
-        });
-});
+    });
+};
